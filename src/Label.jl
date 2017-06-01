f1b60838a617df73bf1965cea5203be3ba952609
Andy Ferris <andy.ferris@roames.com.au>
2017-06-01 10:52:04 +1000

    More compact printing of `Label`s

--- src/Label.jl
+++ src/Label.jl
@@ -10,3 +10,4 @@ end
 @pure (==)(::Label{Name1}, ::Label{Name2}) where {Name1, Name2} = false
 
 show(io::IO, ::Label{Name}) where {Name} = print(io, "l\"$(Name)\"")
+print(io::IO, ::Label{Name}) where {Name} = print(io, Name)
--- src/Tabulars.jl
+++ src/Tabulars.jl
@@ -2,7 +2,7 @@ module Tabulars
 
 import Base: @pure, @propagate_inbounds, @_pure_meta, @_inline_meta, @_propagate_inbounds_meta,
     getindex, setindex!, indices, size, tail, summary, show, length, start, next, done,
-    transpose, permutedims, ndims, show
+    transpose, permutedims, ndims, show, print
 
 import Base: ==
 
