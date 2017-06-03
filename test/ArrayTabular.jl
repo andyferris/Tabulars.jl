209665f80f1b6c6338ce529a32ec4cd57f96ef55
Andy Ferris <andy.ferris@roames.com.au>
2017-06-03 23:17:19 +1000

    A few ArrayTabular tests
     * Need to fix nested Series construction

--- test/ArrayTabular.jl
+++ test/ArrayTabular.jl
@@ -1,21 +1,41 @@
 @testset "ArrayTabular" begin
     @testset "ArraySeries" begin
-        t1 = ArraySeries([2,3,4,5])
-
         @test @inferred(Series([2,3,4,5])) isa ArraySeries
+
+        t1 = Series([2,3,4,5])
 
         @test @inferred(indices(t1)) === (Base.OneTo(4),)
         @test @inferred(t1[3]) == 4
         @test_throws Exception t1[1,2]
 
-        @test t1[:]::ArraySeries == t1
-        @test t1[:]::ArraySeries == t1
+        @test @inferred(t1[:])::ArraySeries == t1
+        @test @inferred(t1[:])::ArraySeries == t1
     end
 
     @testset "ArrayTable" begin
-        t2 = ArrayTable([[1,2,3],[4,5,6]])
+        # Flat ArrayTable
+        @test @inferred(Table([1 4; 2 5; 3 6])) isa ArrayTable{<:Matrix}
 
-        @test indices(t2) === (Base.OneTo(3), Base.OneTo(2))
-        @test t2[2,1] == 2
+        t2 = Table([1 4; 2 5; 3 6])
+
+        @test @inferred(indices(t2)) === (Base.OneTo(3), Base.OneTo(2))
+        @test @inferred(t2[2,1]) === 2
+        @test @inferred(t2[:,1]) == Series([1,2,3])
+        @test @inferred(t2[2,:]) == Series([2,5])
+        @test @inferred(t2[:,:]) == t2
+
+        # Nested ArrayTable
+        #@test @inferred(Table([[1,2,3],[4,5,6]])) isa ArrayTable{<:Vector}
+
+        t3 = Table([[1,2,3],[4,5,6]])
+
+        @test @inferred(indices(t3)) === (Base.OneTo(3), Base.OneTo(2))
+        @test @inferred(t3[2,1]) === 2
+        @test @inferred(t3[:,1]) == Series([1,2,3])
+        @test @inferred(t3[2,:]) == Series([2,5])
+        @test @inferred(t3[:,:]) == t2
+
+        # With other inner types
+        @test_broken @inferred(Table([(:a=>1, :b=>2, :c=>3), (:a=>4, :b=>5, :c=>6)])) isa ArrayTable{<:Vector}
     end
 end
